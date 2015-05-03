de0966229960c12676fcbebc87365e14ab520691
afurikova <afurikova@yahoo.fr>
2015-05-03 21:56:36 +0200

    make some "cosmetic" changes

--- js/script.js
+++ js/script.js
@@ -1,52 +1,41 @@
 $().ready (function() {
     console.log("DOM fully loaded and parsed");
-var initialCats = [
-    {
-        "name" : "Ben",
-        "src" : "http://placekitten.com/g/500/450",
-        "clicks": 0,
-        "nicknames": ["Benik", "Bubak"]
-    },
+    var initialCats = [
+        {
+            "name" : "Ben",
+            "src" : "http://placekitten.com/g/500/450",
+            "clicks": 0,
+            "nicknames": ["Benik", "Bubak"]
+        },
 
-    {
-        "name" : "Dianette",
-        "src" : "http://placekitten.com/g/500/300",
-        "clicks": 0,
-        "nicknames": ["Dian", "Kocka"]
-    },
+        {
+            "name" : "Dianette",
+            "src" : "http://placekitten.com/g/500/300",
+            "clicks": 0,
+            "nicknames": ["Dian", "Kocka"]
+        },
 
-    {
-        "name" : "Micinka",
-        "src" : "http://placekitten.com/g/500/350",
-        "clicks": 0,
-        "nicknames": ["Benik", "Bubak"]
-    },
+        {
+            "name" : "Micinka",
+            "src" : "http://placekitten.com/g/500/350",
+            "clicks": 0,
+            "nicknames": ["Benik", "Bubak"]
+        },
 
-    {
-        "name" : "Simon",
-        "src" : "http://placekitten.com/g/500/400",
-        "clicks": 0,
-        "nicknames": ["Benik", "Bubak"]
-    },
+        {
+            "name" : "Simon",
+            "src" : "http://placekitten.com/g/500/400",
+            "clicks": 0,
+            "nicknames": ["Benik", "Bubak"]
+        },
 
-    {
-        "name" : "Pedro",
-        "src" : "http://placekitten.com/g/500/420",
-        "clicks": 0,
-        "nicknames": ["Benik", "Bubak"]
-    },
-]
-
-//     var myViewModel = {
-//         name: ko.observable("ben"),
-//         img: ko.observable("http://placekitten.com/g/500/450"),
-//         clicks: ko.observable(0),
-//         incrementCounter: function () {
-//             console.log("click")
-//             this.clicks(this.clicks() + 1);
-//         }
-// };
-// ko.applyBindings(myViewModel);
+        {
+            "name" : "Pedro",
+            "src" : "http://placekitten.com/g/500/420",
+            "clicks": 0,
+            "nicknames": ["Benik", "Bubak"]
+        },
+    ]
 
     var Cat = function (data) {
         this.name = ko.observable(data.name);
@@ -56,15 +45,15 @@ var initialCats = [
 
         this.level = ko.computed(function () {
             if (this.clicks() < 5) {
-                return "Newborn"
+                return "Newborn";
             } else if (this.clicks() < 10) {
-                return "Clid"
+                return "Clid";
             } else if (this.clicks() < 15) {
-                return "Teen"
+                return "Teen";
             } else if (this.clicks() < 20) {
-                return "Adult"
+                return "Adult";
             } else {
-                return "Nearly dead"
+                return "Nearly dead";
             }          
         }, this);
         
@@ -75,22 +64,21 @@ var initialCats = [
         self.catList = [];
         //console.log(catList)
         initialCats.forEach(function(catItem){
-            self.catList.push(new Cat(catItem))
-        })
+            self.catList.push(new Cat(catItem));
+        });
         //console.log(catList)
         self.currentCat = ko.observable(self.catList[0]);
 
         this.incrementCounter = function () {
             //console.log(this.clicks())
-            this.clicks(this.clicks() + 1)
+            this.clicks(this.clicks() + 1);
             // this.currentCat().clicks(this.currentCat().clicks() + 1);
         };
         this.changeCurrentCat = function (cat) {
             //console.log(cat.name())
-            self.currentCat(cat)
-        }
-    }
+            self.currentCat(cat);
+        };
+    };
 
     ko.applyBindings(new ViewModel());
-
- });
+});
