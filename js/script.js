3853db747401fe7b8b6ad549d59abd84b5a28a7d
afurikova <afurikova@yahoo.fr>
2015-07-28 14:52:36 +0200

    add admin mode

--- js/script.js
+++ js/script.js
@@ -5,35 +5,35 @@ $().ready (function() {
             "name" : "Ben",
             "src" : "http://placekitten.com/g/500/450",
             "clicks": 0,
-            "nicknames": ["Benik", "Bubak"]
+            "nicknames": ["Benny", "Bubu"]
         },
 
         {
             "name" : "Dianette",
             "src" : "http://placekitten.com/g/500/300",
             "clicks": 0,
-            "nicknames": ["Dian", "Kocka"]
+            "nicknames": ["Dian", "Litle Bitch"]
         },
 
         {
             "name" : "Micinka",
             "src" : "http://placekitten.com/g/500/350",
             "clicks": 0,
-            "nicknames": ["Benik", "Bubak"]
+            "nicknames": ["Mici"]
         },
 
         {
             "name" : "Simon",
             "src" : "http://placekitten.com/g/500/400",
             "clicks": 0,
-            "nicknames": ["Benik", "Bubak"]
+            "nicknames": ["Sisi", "Cat"]
         },
 
         {
             "name" : "Pedro",
             "src" : "http://placekitten.com/g/500/420",
             "clicks": 0,
-            "nicknames": ["Benik", "Bubak"]
+            "nicknames": ["Pepik"]
         },
     ]
 
@@ -45,15 +45,19 @@ $().ready (function() {
 
         this.level = ko.computed(function () {
             if (this.clicks() < 5) {
-                return "Newborn";
-            } else if (this.clicks() < 10) {
-                return "Clid";
+                return "newborn";
             } else if (this.clicks() < 15) {
-                return "Teen";
-            } else if (this.clicks() < 20) {
-                return "Adult";
+                return "child";
+            }else if (this.clicks() < 32) {
+                return "teen";
+            }else if (this.clicks() < 51) {
+                return "young(wo)man";
+            } else if (this.clicks() < 74) {
+                return "adult";
+            } else if (this.clicks() < 99) {
+                return "old";
             } else {
-                return "Nearly dead";
+                return "nearly dead";
             }          
         }, this);
         
@@ -61,6 +65,7 @@ $().ready (function() {
 
     var ViewModel = function () {
         var self = this;
+        // self.adminMode = ko.observable(false);
         self.catList = [];
         //console.log(catList)
         initialCats.forEach(function(catItem){
@@ -69,15 +74,28 @@ $().ready (function() {
         //console.log(catList)
         self.currentCat = ko.observable(self.catList[0]);
 
-        this.incrementCounter = function () {
+        self.incrementCounter = function () {
             //console.log(this.clicks())
             this.clicks(this.clicks() + 1);
             // this.currentCat().clicks(this.currentCat().clicks() + 1);
         };
-        this.changeCurrentCat = function (cat) {
+        self.changeCurrentCat = function (cat) {
             //console.log(cat.name())
             self.currentCat(cat);
         };
+        // when click on Admin button, render a form to change data about current cat
+    
+        self.saveData = function (e) {
+            console.log("data saved");
+        }
+
+        self.disableAdminMode = function () {
+            $(".admin").addClass("disable");
+        }
+        self.enableAdminMode = function () {
+            $(".admin").removeClass("disable");
+            // self.adminMode(true);
+        }
     };
 
     ko.applyBindings(new ViewModel());
